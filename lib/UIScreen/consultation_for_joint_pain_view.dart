9016f048e30b069ebfd436349e4881bd64a7bb98
SomabhaiMistry <vaibhav.mis96@gmail.com>
2022-07-19 09:42:03 +0530

    file Updated

--- lib/UIScreen/consultation_for_joint_pain_view.dart
+++ lib/UIScreen/consultation_for_joint_pain_view.dart
@@ -19,6 +19,12 @@ class ConsultationForJointPainView extends StatefulWidget {
 
 class _ConsultationForJointPainViewState extends State<ConsultationForJointPainView> {
   List<ConsultationModel> consultationList = [
+    ConsultationModel(img_food1,"Dr. 123 Patel","Orthopedist","9 Years","455","4:30 pm"),
+    ConsultationModel(img_food2,"Dr. Niraj Patel","Orthopedist","9 Years","455","4:30 pm"),
+    ConsultationModel(img_food3,"Dr. Niraj Patel","Orthopedist","9 Years","455","4:30 pm"),
+    ConsultationModel(img_food1,"Dr. Niraj Patel","Orthopedist","9 Years","455","4:30 pm"),
+    ConsultationModel(img_food2,"Dr. Niraj Patel","Orthopedist","9 Years","455","4:30 pm"),
+    ConsultationModel(img_food3,"Dr. Niraj Patel","Orthopedist","9 Years","455","4:30 pm"),
     ConsultationModel(img_food1,"Dr. Niraj Patel","Orthopedist","9 Years","455","4:30 pm"),
     ConsultationModel(img_food2,"Dr. Niraj Patel","Orthopedist","9 Years","455","4:30 pm"),
     ConsultationModel(img_food3,"Dr. Niraj Patel","Orthopedist","9 Years","455","4:30 pm"),
@@ -42,111 +48,114 @@ class _ConsultationForJointPainViewState extends State<ConsultationForJointPainV
             ),textAlign: TextAlign.center,),
           ),
 
-          ListView.builder(
-            scrollDirection: Axis.vertical,
-            primary: false,
-            shrinkWrap: true,
-            itemCount: consultationList.length,
-            itemBuilder: (context, i) =>
-                Container(
-                  margin: EdgeInsets.only(left: 14.w,right: 14.w,top: 15.h),
-                  decoration: boxDecoration,
-                  child: Column(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Container(
-                        margin: EdgeInsets.symmetric(vertical: 14.h,horizontal: 14.w),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          mainAxisSize: MainAxisSize.max,
-                          children: [
-                            SizedBox(
-                              width: 85.w,
-                              height: 96.h,
-                              child: ClipRRect(
-                                borderRadius: BorderRadius.circular(10.r),
-                                child: Image.asset(consultationList[i].imgDoctor,width: 85.w, height: 96.h,fit: BoxFit.fill,),
-                              ),
-                            ),
-
-                            SizedBox(width: 14.w,),
-
-                            Column(
-                              mainAxisAlignment: MainAxisAlignment.start,
-                              crossAxisAlignment: CrossAxisAlignment.start,
-                              children: [
-                                Text(consultationList[i].tvDoctorName,style: TextStyle(
-                                    fontSize: 17.sp,color: Colors.black,fontFamily: circularXXTTTrialMedium
-                                ),),
-                                SizedBox(height: 4.h,),
-                                Text(consultationList[i].tvSpecialist,style: TextStyle(
-                                    fontSize: 12.sp,color: silver_67696c,fontFamily: circularXXTTTrialRegular
-                                ),),
-                                SizedBox(height: 12.h,),
-                                Text(str_exp+consultationList[i].tvExperience,style: TextStyle(
-                                    fontSize: 12.sp,color: grays_424448
-                                ),),
-                                SizedBox(height: 3.h,),
-                                Row(
-                                  mainAxisAlignment: MainAxisAlignment.start,
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                    Text(consultationList[i].tvConsultations+str_consultations,style: TextStyle(
-                                        fontSize: 12.sp,color: silver_67696c,fontFamily: circularXXTTTrialRegular
-                                    ),),
-                                    SizedBox(width: 4.w,),
-                                    Image.asset(img_like,width: 20.w,height: 18.h,)
-                                  ],
+          Expanded(
+            child: ListView.builder(
+              scrollDirection: Axis.vertical,
+              primary: false,
+              padding: EdgeInsets.only(top: 21.h),
+              shrinkWrap: true,
+              itemCount: consultationList.length,
+              itemBuilder: (context, i) =>
+                  Container(
+                    margin: EdgeInsets.only(left: 14.w,right: 14.w,bottom: 15.h),
+                    decoration: boxDecoration,
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Container(
+                          margin: EdgeInsets.symmetric(vertical: 14.h,horizontal: 14.w),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            mainAxisSize: MainAxisSize.max,
+                            children: [
+                              SizedBox(
+                                width: 85.w,
+                                height: 96.h,
+                                child: ClipRRect(
+                                  borderRadius: BorderRadius.circular(10.r),
+                                  child: Image.asset(consultationList[i].imgDoctor,width: 85.w, height: 96.h,fit: BoxFit.fill,),
                                 ),
-                              ],
-                            )
-                          ],
+                              ),
+
+                              SizedBox(width: 14.w,),
+
+                              Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(consultationList[i].tvDoctorName,style: TextStyle(
+                                      fontSize: 17.sp,color: Colors.black,fontFamily: circularXXTTTrialMedium
+                                  ),),
+                                  SizedBox(height: 4.h,),
+                                  Text(consultationList[i].tvSpecialist,style: TextStyle(
+                                      fontSize: 12.sp,color: silver_67696c,fontFamily: circularXXTTTrialRegular
+                                  ),),
+                                  SizedBox(height: 12.h,),
+                                  Text(str_exp+consultationList[i].tvExperience,style: TextStyle(
+                                      fontSize: 12.sp,color: grays_424448
+                                  ),),
+                                  SizedBox(height: 3.h,),
+                                  Row(
+                                    mainAxisAlignment: MainAxisAlignment.start,
+                                    crossAxisAlignment: CrossAxisAlignment.start,
+                                    children: [
+                                      Text(consultationList[i].tvConsultations+str_consultations,style: TextStyle(
+                                          fontSize: 12.sp,color: silver_67696c,fontFamily: circularXXTTTrialRegular
+                                      ),),
+                                      SizedBox(width: 4.w,),
+                                      Image.asset(img_like,width: 20.w,height: 18.h,)
+                                    ],
+                                  ),
+                                ],
+                              )
+                            ],
+                          ),
                         ),
-                      ),
-                      Container(
-                        height: 1,
-                        width: double.infinity,
-                        color: line_gray_e2e2e6,
-                      ),
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceAround,
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
+                        Container(
+                          height: 1,
+                          width: double.infinity,
+                          color: line_gray_e2e2e6,
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceAround,
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
 
-                          Expanded(
-                            flex: 1,
-                            child: Container(
-                              padding: EdgeInsets.symmetric(vertical: 16.h),
-                              child: Text(str_next_available+consultationList[i].tvNextTime,style: TextStyle(
-                                  fontFamily: interSemibold,color: bg_btn_199a8e,fontSize: 13.sp
-                              ),
-                                textAlign: TextAlign.center,),
-                            ),
-                          ),
-                          InkWell(
-                            onTap: () {
-                              selectConsultation(context);
-                            },
-                            child: Container(
-                              decoration: BoxDecoration(
-                                color:bg_btn_199a8e,
-                                borderRadius: BorderRadius.only(
-                                    bottomRight: Radius.circular(20.r)
+                            Expanded(
+                              flex: 1,
+                              child: Container(
+                                padding: EdgeInsets.symmetric(vertical: 16.h),
+                                child: Text(str_next_available+consultationList[i].tvNextTime,style: TextStyle(
+                                    fontFamily: interSemibold,color: bg_btn_199a8e,fontSize: 13.sp
                                 ),
+                                  textAlign: TextAlign.center,),
                               ),
-                              padding: EdgeInsets.symmetric(vertical: 16.h,horizontal: 15.w),
-                              child: Text(str_consult_now,style: TextStyle(
-                                  fontFamily: interBold,color: Colors.white,fontSize: 13.sp
-                              ),
-                                textAlign: TextAlign.center,),
                             ),
-                          ),
-                        ],
-                      )
-                    ],
-                  ),
-                )
+                            InkWell(
+                              onTap: () {
+                                selectConsultation(context);
+                              },
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color:bg_btn_199a8e,
+                                  borderRadius: BorderRadius.only(
+                                      bottomRight: Radius.circular(15.r)
+                                  ),
+                                ),
+                                padding: EdgeInsets.symmetric(vertical: 16.h,horizontal: 15.w),
+                                child: Text(str_consult_now,style: TextStyle(
+                                    fontFamily: interBold,color: Colors.white,fontSize: 13.sp
+                                ),
+                                  textAlign: TextAlign.center,),
+                              ),
+                            ),
+                          ],
+                        )
+                      ],
+                    ),
+                  )
+            ),
           ),
         ],
       ),
